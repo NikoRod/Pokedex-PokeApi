d102d020829bb2662e84c750d32ce8e7da914828
Nikolas Rodriguez <nikolasRI780mail.com>
2023-07-24 23:28:21 -0300

    gh-pages add

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if your are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:NikoRod/Pokedex.git main:gh-pages
+# git push -f git@github.com:NikoRod/Pokedex-PokeApi.git main:gh-pages
